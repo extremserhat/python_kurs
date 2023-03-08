66e910b44429bbdffaff5a79bd8eed10d0620716
Serhat Leylek <Serhat.Leylek@gfn.education>
2023-03-08 14:28:56 +0100

    neues

--- Lessons/day3/comparisons_oparators.py
+++ Lessons/day3/comparisons_oparators.py
@@ -14,4 +14,30 @@ comparisons operator:
 Equal:                  3 == 2
 Not Equal:              3 != 2
 Greater Than:           3 > 2 
+Less Than:              3 < 2
+Greater or Equal:       3 >= 2
+Less or Equal:          3 <= 2
 """
+num_1 = 3
+num_2 = 2
+print(terminal_color.HEADER + terminal_color.UNDERLINE + "Comparisons Operator:" + terminal_color.ENDCOLOR)
+print(f"is {terminal_color.GREEN + str(num_1) + terminal_color.ENDCOLOR}\t Equal \t\t\t{terminal_color.GREEN + str(num_2) + terminal_color.ENDCOLOR} =\t ", num_1 == num_2)
+
+print(f"is {terminal_color.GREEN + str(num_1) + terminal_color.ENDCOLOR}\t Not Equal \t\t{terminal_color.GREEN + str(num_2) + terminal_color.ENDCOLOR} =\t ", num_1 != num_2)
+
+print(f"is {terminal_color.GREEN + str(num_1) + terminal_color.ENDCOLOR}\t Greater Than \t\t{terminal_color.GREEN + str(num_2) + terminal_color.ENDCOLOR} =\t ", num_1 > num_2)
+
+print(f"is {terminal_color.GREEN + str(num_1) + terminal_color.ENDCOLOR}\t Less Than \t\t{terminal_color.GREEN + str(num_2) + terminal_color.ENDCOLOR} =\t ", num_1 < num_2)
+
+print(f"is {terminal_color.GREEN + str(num_1) + terminal_color.ENDCOLOR}\t Greater or Equal \t{terminal_color.GREEN + str(num_2) + terminal_color.ENDCOLOR} =\t ", num_1 >= num_2)
+
+print(f"is {terminal_color.GREEN + str(num_1) + terminal_color.ENDCOLOR}\t Less or Equal \t\t{terminal_color.GREEN + str(num_2) + terminal_color.ENDCOLOR} =\t ", num_1 <= num_2)
+
+# cast string to int
+num_3 = '100'
+num_4 = '200'
+
+print(f"cast the '{num_3}'" + terminal_color.GREEN + " string " + terminal_color.ENDCOLOR + "to " + terminal_color.CYAN + "int" + terminal_color.ENDCOLOR + f" and '{num_4}'" + terminal_color.GREEN + " string " + terminal_color.ENDCOLOR + "to " + terminal_color.CYAN + "int " + terminal_color.ENDCOLOR + "for addition and the result should be", int(num_3) + int(num_4))
+
+
+
