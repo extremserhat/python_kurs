64275a4dddefbc7d7bade75d7c70c1c49746d952
Serhat Leylek <Serhat.Leylek@gfn.education>
2023-03-08 13:26:51 +0100

    neues gelerntes + day4

---
+++ Lessons/day3/comparisons_oparators.py
@@ -0,0 +1,17 @@
+class terminal_color:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDCOLOR = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+"""
+comparisons operator:
+Equal:                  3 == 2
+Not Equal:              3 != 2
+Greater Than:           3 > 2 
+"""
--- Lessons/day3/inter_and_floats.py
+++ Lessons/day3/inter_and_floats.py
@@ -15,4 +15,53 @@ Modulus:            3 % 2
 
 # 
 # """
-# print(3 / 2)
+class terminal_color:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDCOLOR = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(terminal_color.HEADER, terminal_color.UNDERLINE +"Arithmetic Oparators:" + terminal_color.ENDCOLOR)
+"""
+Addition:           3 + 3
+Subtraction:        3 - 2
+Multiplication:     3 * 3
+Division:           3 / 2
+Floor_Division:     3 // 2
+Exponent:           3 ** 3
+Modulus:            3 % 2
+"""
+a = "3 + 2"
+b = "3 - 2"
+c = "3 / 2"
+d = "3 * 2"
+e = "3 // 2"
+f = "3 ** 2"
+g = "3 % 2"
+print(f"Addition: {terminal_color.BLUE + a + terminal_color.ENDCOLOR} = ", 3 + 2)
+print(f"Subtraction:  {terminal_color.BLUE + b + terminal_color.ENDCOLOR} =", 3 - 2)
+print(f"Division: {terminal_color.BLUE + b + terminal_color.ENDCOLOR} = ", 3 / 2)
+print(f"Multiplication: {terminal_color.BLUE + d + terminal_color.ENDCOLOR} = ", 3 * 2)
+print(f"Floor Division: {terminal_color.BLUE + e + terminal_color.ENDCOLOR} = ", 3 // 2)
+print(f"Exponent: {terminal_color.BLUE + f + terminal_color.ENDCOLOR} = ", 3 ** 2)
+print(f"Modulus: {terminal_color.BLUE + g + terminal_color.ENDCOLOR} = ", 3 % 2)
+
+h = "3 * (2 + 1)"
+print(f"Addition: {terminal_color.BLUE + h + terminal_color.ENDCOLOR} = ", 3 * (2 + 1))
+
+num2 =  1
+# num2 = num2 + 1
+# short way syntax
+num2 += 1
+print(f"num + 1 = {num2}")
+
+# absolute value
+print("absolute value of -3: ",abs(-3))
+
+# round
+print("round a float number 3.75: ", round(3.75))
